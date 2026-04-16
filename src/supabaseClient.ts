529327b8d477e9e58a97b3085efd8f8e849d42ab
Arti <artjom.brikmann@googlemail.com>
2026-04-16 16:35:07 +0200

    Save bitte

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,9 +1,8 @@
 // lib/supabase.js
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(
+export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
 
-export default supabase
