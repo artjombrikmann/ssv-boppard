bb3cd96f10e459a95c3ae0d4021c172e11e1c9a2
Arti <artjom.brikmann@googlemail.com>
2026-04-16 17:03:58 +0200

    Supabase unsicher

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,8 +1,7 @@
-// lib/supabase.js
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
+const SUPABASE_URL = "https://kfawjltveddyoqewayzh.supabase.co";
+const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtmYXdqbHR2ZWRkeW9xZXdheXpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyNjY0NzMsImV4cCI6MjA4OTg0MjQ3M30._oCJg10y_0dyCalOsSiHQ9PZUwW2dZV-2IcseWrWg9k";
 
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
